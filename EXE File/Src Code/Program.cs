c18be6c0a38573cd48071213b4dab0cf27a5a836
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-05-30 20:56:52 +0300

    HWID Show(EXE)

---
+++ EXE File/Src Code/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace HWIDShow
+{
+    class Program
+    {
+        [DllImport(@"ConsoleHider.dll")]
+        public static extern void ConsoleHide();
+        static void Main(string[] args)
+        {
+            ConsoleHide();
+            MessageBox.Show("Showing HWID...");
+            var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            var g = key.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography", true);
+            var j = g.GetValue("MachineGuid");
+            MessageBox.Show(j.ToString().Replace("-", String.Empty).ToString());
+            Environment.Exit(665);
+        }
+    }
+}
